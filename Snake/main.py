e2bfbcc90b42ce8807d03240a8392083d2bea0ce
Xardas1 <ciufel.maly@gmail.com>
2024-04-10 19:46:15 +0200

    guwno_mapa_zhandlowana

--- Snake/main.py
+++ Snake/main.py
@@ -1,10 +1,12 @@
 import pygame
 
+
 def init(screen_size):
     pygame.init()
     pygame.display.set_caption("Snake")
     screen = pygame.display.set_mode(screen_size)
     return screen
+    
 
 def check_for_quit(Events):
     for event in Events:
@@ -18,21 +20,22 @@ def check_for_quit(Events):
 def get_input_events():
     return pygame.event.get()
 
+
+
+
 def main():
+    map = pygame.transform.scale(pygame.image.load("Game_Images/Map.jpg"), (1280, 720))
+    screen_size = (1280, 720)
+    screen = init(screen_size)
+    
+
     while True:
-        init((1280, 720))
         Events = get_input_events()
         check_for_quit(Events)
+        screen.blit(map, (0,0))
         if check_for_quit(Events):
             break
-
-
-
-
-
-
-
-
+        pygame.display.flip()
 
 
 
